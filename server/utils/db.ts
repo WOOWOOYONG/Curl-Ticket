146f394b07abfbb92d47725cf74dcecf0052f88f
WOOWOOYONG <woowooyong@gmail.com>
2026-02-03 11:07:58 +0800

    fix: 修正 server utils import

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
-import * as schema from '~~/server/database/schema'
+import * as schema from '../database/schema'
 
 // 使用 lazy singleton 模式，確保只建立一個連線
 let _db: ReturnType<typeof drizzle<typeof schema>> | null = null
@@ -14,4 +14,4 @@ export function useDB() {
   return _db
 }
 
-export * from '~~/server/database/schema'
+export * from '../database/schema'
